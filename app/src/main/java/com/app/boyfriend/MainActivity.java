747821fbae6e04ba4a3b12d97aa11269e85fb247
junecubes <junecubes@gmail.com>
2019-08-04 02:11:31 +0530

    3

--- app/src/main/java/com/app/boyfriend/MainActivity.java
+++ app/src/main/java/com/app/boyfriend/MainActivity.java
@@ -3,11 +3,21 @@ package com.app.boyfriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.startapp.android.publish.adsCommon.StartAppAd;
+import com.startapp.android.publish.adsCommon.StartAppSDK;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StartAppSDK.init(this, "207068989", true);
+    }
+
+    public void onBackPressed() {
+        StartAppAd.onBackPressed(this);
+        super.onBackPressed();
     }
 }
